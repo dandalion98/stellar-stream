61d781e7bc70c3c1ae5f07c8aae9586ba74043ef
dan <dandalion98@gmail.com>
2018-06-01 20:37:33 -0400

    format trade data

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ async function main() {
 let lastTradeId;
 var server = new StellarSdk.Server(config.stellarServer);
 
+let accounts = new Set()
+accounts.add("GAGX6QWJNQ22UO4VU6AOJV65SXRKWUJVZ3FOZNZYYF4FWKA5VCUW7W5W")
+// C1
+accounts.add("GC3IUU5LCEAQ5T4NV236TQ6AR5V3YRS3HADI5JTXTIAO2AAEG4Y25R3Y")
+// C2
+accounts.add("GCTPYCIMKYKY7HFXHLTHKPZ2543XGIZG2B3IFIWYTTA2GJIN7F37R73A")
+
 var KeyedMessage = kafka.KeyedMessage;
 var Client = kafka.Client;
 
@@ -57,6 +64,7 @@ kconsumer.on('message', function (message) {
 
 
 async function initKafka() {
+    log.info("initializing Kafka")
     return new Promise((resolve, reject) => {
         kclient.on('ready', async () => {
             resolve();
@@ -67,6 +75,7 @@ async function initKafka() {
 async function fetchTrades() {
     console.log("fetching")
     var trades = await server.trades().order('desc').limit(50).call()
+    console.log("fetched")
     let firstTradeId
     let payload = []
     for (let trade of trades.records) {
@@ -79,21 +88,55 @@ async function fetchTrades() {
             firstTradeId = trade.id;
         }
 
-        let record = _.pick(trade, ["base_account", 
-                                    "base_amount", 
-                                    "base_asset_type",
-                                    "counter_account",
-                                    "counter_amount",
-                                    "counter_asset_type",
-                                    "counter_asset_code",
-                                    "counter_asset_issuer",
-                                    "base_is_seller",
-                                    "price"])
-        payload.push({ topic: config.kafka.outputTradesTopic, messages: JSON.stringify(record)})
+        // let record = _.pick(trade, ["base_account", 
+        //                             "base_amount", 
+        //                             "base_asset_type",
+        //                             "counter_account",
+        //                             "counter_amount",
+        //                             "counter_asset_type",
+        //                             "counter_asset_code",
+        //                             "counter_asset_issuer",
+        //                             "base_is_seller",
+        //                             "price"])
+        let record
+        if (accounts.has(trade.base_account)) {
+            record = {
+                fromAccount: trade.base_account,
+                fromAmount: trade.base_amount,
+                fromAssetCode: trade.base_asset_code,                                
+                fromAssetType: trade.base_asset_type,
+                fromAssetIssuer: trade.base_asset_issuer,
+                toAmount: trade.counter_amount,
+                toAssetType: trade.counter_asset_type,
+                toAssetCode: trade.counter_asset_code,                                
+                toAssetIssuer: trade.counter_asset_issuer
+            }
+            console.dir(record)
+            payload.push({ topic: config.kafka.outputTradesTopic, messages: JSON.stringify(record) })
+        } 
+        
+        if (accounts.has(trade.counter_account)) {
+            record = {
+                fromAccount: trade.counter_account,
+                fromAmount: trade.counter_amount,
+                fromAssetCode: trade.counter_asset_code,                                
+                fromAssetType: trade.counter_asset_type,
+                fromAssetIssuer: trade.counter_asset_issuer,
+                toAmount: trade.base_amount,
+                toAssetType: trade.base_asset_type,
+                toAssetCode: trade.base_asset_code,                                                
+                toAssetIssuer: trade.base_asset_issuer
+            }
+            console.dir(record)            
+            payload.push({ topic: config.kafka.outputTradesTopic, messages: JSON.stringify(record) })            
+        }
     }
 
     if (firstTradeId) {
-        lastTradeId = firstTradeId
+        lastTradeId = firstTradeId      
+    }
+
+    if (payload.length > 0) {
         await ksend(payload)
     }
 
